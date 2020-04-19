0f312348f52a3bc5003b2617a87a263200e236f9
Herbert Poul <herbert@codeux.design>
2020-04-19 14:31:54 +0200

    update jodd-beans and ratpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     id("org.jetbrains.kotlin.jvm").version("1.3.20")
     //kotlin("kapt") version "1.3.20"
-    id("io.ratpack.ratpack-java").version("1.7.3")
+    id("io.ratpack.ratpack-java").version("1.7.6")
 
     // Apply the application plugin to add support for building a CLI application.
     application
